97bfdf6a8d8c2e22835cf40546901c59705fc106
hyoungwonkang <hwplus@gmail.com>
2022-04-24 01:55:46 +0900

    typescript array

---
+++ typescript/1-types/1-3-array.ts
@@ -0,0 +1,8 @@
+{
+  // Array
+  const fruits: string[] = ["apple", "banana"];
+  const scores: Array<number> = [1, 3, 4];
+  function printArray(fruits: readonly string[]) {
+    // readonly를 사용하려면 string 다음에 배열 표시가 있는 문법이용.
+  }
+}
