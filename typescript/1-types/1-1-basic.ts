45628bd7c128e9404795532977203ebc570c63e9
hyoungwonkang <hwplus@gmail.com>
2022-04-24 21:22:03 +0900

    수정

--- typescript/1-types/1-1-basic.ts
+++ typescript/1-types/1-1-basic.ts
@@ -11,9 +11,9 @@
   // boolean
   const boal: boolean = false;
 
-  //undefinde
-  // let name: undefined;
-  // name='hello';
+  //undefined      
+  // let name: undefined;   
+  // name='hello';      똥!
   let age: number | undefined; // 옵셔널 타입
   age = undefined;
   age = 1;
@@ -23,18 +23,41 @@
     // return undefined;       //undefined를 리턴합니다!
   }
 
-  //null
-  // let person: null;
+  //null       
+  // let person: null;      
   // person = null;
-  // person = 1;
+  // person = 1;           똥!
   let person2: string | null;
 
-  //unknown       왠만하면 쓰지말기!
+  //unknown       똥!    어떤것이 들어올지 모르겠어!
   let notSure: unknown = 0;
   notSure = "he";
   notSure = true;
 
-  //any       왠만하면 쓰지말기!
+  //any       똥!    어떤것이든 다 담을 수 있어!
   let anything: any = 0;
   anything = "hello";
+
+  //void        비어있는 것 표현!
+  function print(): void {      //: void 생략 가능
+      console.log('hello');
+      return;       // 리턴값이 없을 때 씀.
+  }
+  let unusable: void = undefined;   //  활용성이 없어 이렇게 선언하지 않는다!
+
+  // never       절대 다른것을 리턴할 수 없다.   
+  function throwError(message:string): never {
+      //message -> server (log)
+      throw new Error(message);
+    while(true){}       //무한 루프
+    // return 1;
+  }
+//   let neverEnding: never;         절대 사용하지 않음
+
+  //object      똥!
+  let obj: object
+  function acceptSomeObject(obj: object) {}
+  acceptSomeObject({name: 'ellie'})
+  acceptSomeObject({animal: 'dog'})
+
 }
