8ac3d09f6dbbf943024a188868915f5b00ac2ee4
hyoungwonkang <hwplus@gmail.com>
2022-04-25 13:31:17 +0900

    TS. 기본 타입 문제

--- typescript/2typesprojects-210120-194902/2-types-projects-quiz/game.ts
+++ typescript/2typesprojects-210120-194902/2-types-projects-quiz/game.ts
@@ -1,12 +1,34 @@
 /**
  * Let's make a game 🕹
  */
+const position = { x: 0, y: 0 };
+type Motion = "up" | "down" | "left" | "right";
+function move(motion: Motion) {
+  switch (motion) {
+    case "up":
+      // return position.y=+1;        //여기서 바로 return 되지 않는다.
+      position.y += 1;
+      break;
+    case "down":
+      position.y -= 1;
+      break;
+    case "left":
+      position.x -= 1;
+      break;
+    case "right":
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`unknown motion: ${motion}`);
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
