0d496a053734e69583b59eff792e98973d2d71a2
Luke Hyoungwon Kang <101075382+hyoungwonkang@users.noreply.github.com>
2022-06-15 23:00:33 +0900

    수정 보완

--- typescript/1-types/1-1-basic.ts
+++ typescript/1-types/1-1-basic.ts
@@ -11,9 +11,9 @@
   // boolean
   const boal: boolean = false;
 
-  //undefined      
-  // let name: undefined;   
-  // name='hello';      똥!
+  //undefined
+  // let name: undefined;
+  // name='hello';  💩
   let age: number | undefined; // 옵셔널 타입
   age = undefined;
   age = 1;
@@ -23,41 +23,40 @@
     // return undefined;       //undefined를 리턴합니다!
   }
 
-  //null       
-  // let person: null;      
+  //null
+  // let person: null;
   // person = null;
-  // person = 1;           똥!
+  // person = 1;   💩
   let person2: string | null;
 
-  //unknown       똥!    어떤것이 들어올지 모르겠어!
+  //unknown   💩    어떤것이 들어올지 모르겠어!
   let notSure: unknown = 0;
   notSure = "he";
   notSure = true;
 
-  //any       똥!    어떤것이든 다 담을 수 있어!
+  //any   💩    어떤것이든 다 담을 수 있어!
   let anything: any = 0;
   anything = "hello";
 
   //void        비어있는 것 표현!
-  function print(): void {      //: void 생략 가능
-      console.log('hello');
-      return;       // 리턴값이 없을 때 씀.
+  function print(): void {
+    //: void 생략 가능
+    console.log("hello");
+    return; // 리턴값이 없을 때 씀.
   }
-  let unusable: void = undefined;   //  활용성이 없어 이렇게 선언하지 않는다!
+  let unusable: void = undefined; //  활용성이 없어 이렇게 선언하지 않는다!
 
-  // never       절대 다른것을 리턴할 수 없다.   
-  function throwError(message:string): never {
-      //message -> server (log)
-      throw new Error(message);
-    while(true){}       //무한 루프
-    // return 1;
+  // never       절대 다른것을 리턴할 수 없다.
+  function throwError(message: string): never {
+    //message -> server (log)
+    throw new Error(message);
+    while (true) {} //무한 루프로 끝내지 않게 한다.
   }
-//   let neverEnding: never;         절대 사용하지 않음
+  //   let neverEnding: never;         절대 사용하지 않음
 
-  //object      똥!
-  let obj: object
+  //object   💩 원시타입을 제외한 모든 객체타입은 가능. 그리하여 사용하지 않음.
+  let obj: object;
   function acceptSomeObject(obj: object) {}
-  acceptSomeObject({name: 'ellie'})
-  acceptSomeObject({animal: 'dog'})
-
+  acceptSomeObject({ name: "ellie" });
+  acceptSomeObject({ animal: "dog" });
 }
