8b4f8d9bf5ec4ac07ad56fdb2078865b8f2bca58
Luke Hyoungwon Kang <101075382+hyoungwonkang@users.noreply.github.com>
2022-06-26 22:12:12 +0900

    JS 빼고 TS만

--- typescript/1-types/1-2-function.ts
+++ typescript/1-types/1-2-function.ts
@@ -1,58 +1,37 @@
 {
-  //JavaScript 💩
-  function jsAdd(num1, num2) {
-    return num1 + num2;
-  }
+  // 타입스크립트의 기본적인 함수에 타입 먹히기
+  // function add(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
 
-  //TypeScript ✨
-  function add(num1: number, num2: number): number {
-    return num1 + num2;
-  }
+  // function fetchNum(id: string): Promise<number> {
+  //   //code...
 
-  //JavaScript 💩
-  function jsFetchNum(id) {
-    // code...
-    // code...
-    // code...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
-  }
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
 
-  //TypeScript ✨
-  //안정적인 개발을 위한 static함을 사용. 문서화에 도움이 됨.
-  function fetchNum(id: string): Promise<number> {
-    // code...
-    // code...
-    // code...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
-  }
 
-  // 자바 스크립트에서도 사용할 수 있고 타스에서 더 활용도를 높일 수 있는 타입이 더해진 함수 정의 방법
-  //Optional parameter     //인자를 전달해도 되고 안해도 된다.        //이름을 출력할 수 있는 함수
+
+  // 옵셔널 파라미터
+  // function printName(firstName: string, lastName: string | undefined) {   //정확하게 undefined로 적어야 한다.는 단점이 있다.
   function printName(firstName: string, lastName?: string) {
     console.log(firstName);
     console.log(lastName);
   }
-  printName("Steve", "Jobs");
-  printName("Ellie"); // 인자 갯수와 정해진 타입으로 전해줘야 한다.
-  //   printName("Anna", undefined);
+  // printName("Anna", undefined);
+  printName("Luke");
 
-  //Default Parameter
-  function printMessage(message: string = "default message") {
-    //타입 스크립트에서 default값 전달 가능.
+  // 디폴트 파라미터
+  function printMassage(message: string = "default message") {
     console.log(message);
   }
-  printMessage();
+  printMassage();
 
-  //Rest parameter
+  //레스트 파라미터
   function addNumbers(...numbers: number[]): number {
-    // ... (rest param)로 numbers라는 배열을 받아옴. number[]: 숫자타입의 배열.
     return numbers.reduce((a, b) => a + b);
   }
   console.log(addNumbers(1, 2));
-  console.log(addNumbers(1, 2, 3, 4));
-  console.log(addNumbers(1, 2, 3, 4, 5, 0));
 }
