b9d47fff9bd60e24aea0c3107a79d16baff7f58a
hyoungwonkang <hwplus@gmail.com>
2023-03-26 17:53:01 +0900

    Type Alias  기초 보충

--- typescript/1-types/1-4-alias.ts
+++ typescript/1-types/1-4-alias.ts
@@ -1,26 +1,30 @@
 {
-  //Type Aliases
+  /** Type Aliases
+   */
 
   type Text = string;
-  const name: Text = "ellie"; //문자열만 정의 가능
-  const address: Text = "korea";
-  type Num = number; // 숫자도 정의 가능
+  const name: Text = "luke"; //문자열만 정의 가능하게 됨
+  const address: Text = "cananda";
+  type Num = number; //숫자도 정의 가능함
   type Student = {
-    // 객체도 정의 가능
+    //  객체도 정의 가능
     name: string;
     age: number;
   };
   const student: Student = {
-    // 타입을 지정한 것들만 가능.
-    // animal: 'cat'    // 타입 지정 안해서 안됨.
-    name: "ellie",
+    // 타입을 지정한 것들만 가능
+    // animal: 'dog' //  타입 지정 안해서 안됨.
+    name: "luke",
     age: 12,
   };
 
-  //String Literal Types  // 타입을 문자열로 지정 가능함
+  /** String Literal Types
+  타입을 문자열로 지정 가능함
+  */
+
   type Name = "name";
-  let ellieName: Name; //ellieName이라는 변수가 Name이라는 변수를 따라간다면
-  ellieName = "name"; // name이라는 문자말고는 다른 문자를 따라 갈 수 없다.
+  let lukeName: Name; //lukeName이라는 변수가 Name이라는 변수를 따라간다면
+  lukeName = "name"; //name이라는 문자말고는 다른 문자를 따라 갈 수 없다.
   type JSON = "json";
   const json: JSON = "json";
 
