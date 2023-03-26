da98ac389572c02fa050a31bba748849b470ce34
hyoungwonkang <hwplus@gmail.com>
2023-03-26 17:36:47 +0900

    Array, Tuple  기초 보충

--- typescript/1-types/1-3-array.ts
+++ typescript/1-types/1-3-array.ts
@@ -1,24 +1,23 @@
 {
   // Array
-  const fruits: string[] = ["apple", "banana"];
+  const fruits: string[] = ["🍎", "🍌"];
   const scores: Array<number> = [1, 3, 4];
   function printArray(fruits: readonly string[]) {
-    // 리드온리에 추가하는것 불가능
-    // readonly를 사용하려면 string 다음에 배열 표시가 있는 문법이용.
-    // 즉 불변성 때문에 꺽쇠 <>보다 string[]을 사용하는 경우가 더 많음.
+    // fruits.push 리드온리 때문에 불가능
   }
+  //   // readonly를 사용하려면 string 다음에 배열 표시가 있는 문법이용.
+  //   // 즉 불변성 때문에 꺽쇠 <>보다 string[]을 사용하는 경우가 더 많음.
 
-
-
-  // 튜플
+  // Tuple
   // 서로 다른 타입의 데이터를 배열에 담을 수 있음   //리액트에서 useState를 쓸 때 씁니다!
   // 하지만 Tuple을 쓰면 인덱스를 읽어야 하는 가독성이 떨어짐.
   let student: [string, number];
   student = ["name", 123];
   student[0]; // name
   student[1]; // 123
+  // student.name;
+  // student.age;
 
-
-  //object destructuring을 이용.
+  // 따라서 object destructuring을 이용.
   const [name, age] = student; // student안에 첫번째 값이 name으로 두번째값이 age로 할당 됨.
 }
